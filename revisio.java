1014e5ee14f7307036ef43c9b8837d76695d5f1c
amiindeeq <amiindeeqxussen@gmail.com>
2023-03-31 23:20:26 +0300

    add .

---
+++ revisio.java
@@ -0,0 +1,77 @@
+package Sem5;
+
+public class Jimce {
+	public static void main(String [] args) {
+		int  a = 6;
+		int b = 9;
+		int add = a+b;
+		int sup = a-b;
+		int mult = a*b;
+		
+		System.out.println("Addition between a and b is:" + add);
+		System.out.print(b / a);
+		System.out.println(sup);
+		System.out.println(mult);
+		System.out.println(b% a);
+	}
+}
+
+
+
+package Sem5;
+
+public class Jimce2 {
+  public static void main(String [] args) {
+	  //System.out.println(5>9);
+	  //System.out.println(100 != 101);
+	  //System.out.println(20 >= 22);
+	  //System.out.println(20 <= 22);
+	  int a =30;
+	  int b =40;
+	  
+	  //System.out.println(a!=b);
+	  int i =30;
+	  int k =40;
+	  boolean check  = a>=b;
+	  System.out.print("the is bolean check:"+ check);
+	 // System.out.println(i<=k);
+	  //System.out.println(a>=b);
+	  //System.out.println(a==b);
+  }
+}
+
+
+
+
+
+
+package Sem5;
+
+public class Calculatejimce {
+	public static void main(String [] args) {
+		//int side=8;
+		//int square = side*side;
+		//System.out.println(square);
+		
+		//double a=0.5;
+		//double b=6.0;
+		//double h=7.0;
+		//double area = a*b*h;
+		//System.out.println( area);
+		
+		
+		//double p=3.14;
+		//double r=7;
+		//double circle = p*r*r;
+		//System.out.println("The circle area is :" + circle);
+		
+		boolean a = true;
+		boolean b = false;
+		boolean g = 100>1000;
+		boolean h = 22==44;
+		//System.out.println(a&&b);
+		
+		boolean logicand = a&&b;
+		System.out.println(b||g);
+		}
+}
